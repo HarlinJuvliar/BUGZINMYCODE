318f3d87fb492ca337e89b8759306d6d58e520a9
Liam Saldanha <73840987+HarlinJuvliar@users.noreply.github.com>
2023-07-19 00:44:50 -0400

    Add files via upload
    Somebody stop me.

---
+++ FirstProgram.py
@@ -0,0 +1,21 @@
+first_name = "Liam"
+last_name = "Saldanha"
+full_name = first_name + " " + last_name
+print ("Hello, " + full_name)
+# bars here u know We got tha barz 
+
+current_year = 2023
+birth_year = 2005
+current_age = current_year - birth_year
+print ("you are " + str(current_age) + " years old!")
+# so a str() forces something into a string value - pyth can't print straight numbers 
+yart = 12
+print (yart)
+#it seems that it can print simple values, but not necessarily do both a calculation and print the string
+
+path = 25 
+wtc = 5 
+one_train = 50 
+total_time = path + wtc + one_train
+total_time_hrs = total_time / 60
+print("you're total time will be " + str(total_time) + " minutes, or " + str(total_time_hrs) + " hours!" )
